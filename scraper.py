1f438cafc4e07b7326b3c693d2f01c9581e11448
HeeteshSimon <heeteshsimon@gmail.com>
2020-04-16 07:18:13 +0530

    Added comments

--- scraper.py
+++ scraper.py
@@ -1,7 +1,9 @@
 #import modules
 import requests
 from bs4 import BeautifulSoup
+#To get the URL for scraping
 page = requests.get('https://forecast.weather.gov/MapClick.php?lat=33.96746500000006&lon=-118.25679999999994#.Xpe2vsgzaMo')
+#Parsing the source in html
 soup = BeautifulSoup(page.content, 'html.parser')
 #print(soup)
-print(soup.find_all('a'))
+#print(soup.find_all('a'))
