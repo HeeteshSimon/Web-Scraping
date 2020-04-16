6d2aef00d7e0b091568bbe38370827622ca64588
HeeteshSimon <heeteshsimon@gmail.com>
2020-04-16 07:16:56 +0530

    Found all the links to the html page

--- scraper.py
+++ scraper.py
@@ -3,4 +3,5 @@ import requests
 from bs4 import BeautifulSoup
 page = requests.get('https://forecast.weather.gov/MapClick.php?lat=33.96746500000006&lon=-118.25679999999994#.Xpe2vsgzaMo')
 soup = BeautifulSoup(page.content, 'html.parser')
-print(soup)
+#print(soup)
+print(soup.find_all('a'))
