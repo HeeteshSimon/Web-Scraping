d611549749770e9033dcbeb9295cd2717a01e9ee
HeeteshSimon <heeteshsimon@gmail.com>
2020-04-16 07:11:34 +0530

    Added the variables with links

---
+++ scraper.py
@@ -0,0 +1,6 @@
+#import modules
+import requests
+from bs4 import BeautifulSoup
+page = requests.get('https://forecast.weather.gov/MapClick.php?lat=33.96746500000006&lon=-118.25679999999994#.Xpe2vsgzaMo')
+soup = BeautifulSoup(page.content, 'html-parser')
+
